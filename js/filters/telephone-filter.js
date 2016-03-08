2e93ca185a79e24c8a59fe305ceb8d358e72392d
Mani <im.manikanta@gmail.com>
2016-03-08 11:36:26 +0530

    Telephone format modification

--- js/filters/telephone-filter.js
+++ js/filters/telephone-filter.js
@@ -13,6 +13,16 @@ angular.module('studentcollab')
             var country, city, number;
 
             switch (value.length) {
+                case 3:
+                    return "(" + value + ")";
+                case 4:
+                case 5:
+                    return "(" + value.slice(0, 3) + ") " + value.slice(3, 6) 
+                case 6:
+                case 7:
+                case 8:
+                case 9:
+                    return "(" + value.slice(0, 3) + ") " + value.slice(3, 6) + "-" + value.slice(6);
                 case 10: // +1PPP####### -> C (PPP) ###-####
                     country = 1;
                     city = value.slice(0, 3);
